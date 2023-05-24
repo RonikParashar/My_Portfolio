afeb98b9be416a44a6cfeae2c8efa82366539880
RonikParashar <ronikparashar123@gmail.com>
2023-05-25 02:33:48 +0530

    ..

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -12,7 +12,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src="/../public/assets/skills/html.png"width='64' height='64'  alt="/" />   
+                        <img src="https://github.com/RonikParashar/My_Portfolio/assets/90423810/8c739248-e342-41f0-af7b-50837fcc212c" width='64' height='64'  alt="/" />   
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3>HTML</h3>
@@ -39,7 +39,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src="/../public/assets/skills/javascript.png"width='64' height='64'  alt="/" />
+                        <img src="https://github.com/RonikParashar/My_Portfolio/assets/90423810/b1993c4a-6ad6-4501-b4e2-f0828001f18c" width='64' height='64'  alt="/" />
                             
                         </div>
                         <div className='flex flex-col items-center justify-center'>
@@ -53,7 +53,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src="/../public/assets/skills/react.png" width='64' height='64'  alt="/" />
+                        <img src="https://github.com/RonikParashar/My_Portfolio/assets/90423810/d33f6067-7b92-465f-8bee-701b9386b10c" width='64' height='64'  alt="/" />
                             
                         </div>
                         <div className='flex flex-col items-center justify-center'>
@@ -67,7 +67,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src="/../public/assets/skills/tailwind.png"width='64' height='64'  alt="/" />
+                        <img src="https://github.com/RonikParashar/My_Portfolio/assets/90423810/e5a92b39-29ba-484d-8435-27532e05ce03" width='64' height='64'  alt="/" />
                             
                         </div>
                         <div className='flex flex-col items-center justify-center'>
@@ -81,7 +81,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src="/../public/assets/skills/python.jpg"width='64' height='64'  alt="/" />
+                        <img src="https://github.com/RonikParashar/My_Portfolio/assets/90423810/746da44c-1e47-4660-badb-2a778691f4c6" width='64' height='64'  alt="/" />
                             
                         </div>
                         <div className='flex flex-col items-center justify-center'>
@@ -109,7 +109,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src="/../public/assets/skills/powerbi.jpeg"width='74' height='74'  alt="/" />
+                        <img src="https://github.com/RonikParashar/My_Portfolio/assets/90423810/1da271f5-3682-43dc-a9be-3626086070c0" width='74' height='74'  alt="/" />
                             
                         </div>
                         <div className='flex flex-col items-center justify-center'>
@@ -123,7 +123,7 @@ const Skills = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src="/../public/assets/skills/github.png"width='64' height='64'  alt="/" />
+                        <img src="https://github.com/RonikParashar/My_Portfolio/assets/90423810/956a1989-d00c-4f2d-ab76-1653ed091274" width='64' height='64'  alt="/" />
                             
                         </div>
                         <div className='flex flex-col items-center justify-center'>
